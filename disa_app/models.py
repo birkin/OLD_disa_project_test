a70d059abe2e289c09cb366506988ac881b8f180
birkin <birkin.diana@gmail.com>
2019-11-15 17:33:00 -0500

    starts modeling experimentation.

--- disa_app/models.py
+++ disa_app/models.py
@@ -7,3 +7,14 @@ from django.db import models
 from django.http import HttpResponseRedirect
 
 log = logging.getLogger(__name__)
+
+
+class Person(models.Model):
+    created = models.DateTimeField( auto_now_add=True )
+    modified = models.DateTimeField( auto_now=True )
+    display_name = models.CharField( max_length=190, default='' )
+
+
+
+
+
